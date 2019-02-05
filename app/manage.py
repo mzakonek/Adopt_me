6568d6e5edf7abf41f17bbfe04ef2c5435bba1c8
someone <something@something.com>
2019-02-05 21:00:59 +0100

    first_commit

---
+++ app/manage.py
@@ -0,0 +1,29 @@
+# from flask_script import Manager
+# from flask_migrate import Migrate, MigrateCommand
+#
+# from app import app, db
+#
+# migrate = Migrate(app, db)
+#
+# manager = Manager(app)
+# manager.add_command('db', MigrateCommand)
+#
+#
+# if __name__ == '__main__':
+#     manager.run()
+
+from flask import Flask
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
+
+
+server = Flask(__name__)
+server.config.from_object(Config)
+db.init_app(server)
+
+migrate = Migrate(server, db)
+manager = Manager(server)
+manager.add_command('db', MigrateCommand)
+
+if __name__ == '__main__':
+    manager.run()
