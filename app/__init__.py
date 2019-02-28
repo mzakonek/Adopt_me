f231615a39081ced129f933be222038fd83ecf53
mzakonek <unnamed200@gmail.com>
2019-02-28 14:06:32 +0100

    Reset password request, JWT, mail configuration

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,7 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_mail import Mail
 
 
 app = Flask(__name__)
@@ -14,6 +15,7 @@ login = LoginManager(app)
 login.login_view = 'login'
 db = SQLAlchemy(app)
 migrate = Migrate(app, db, compare_type=True)
+mail = Mail(app)
 
 if not app.debug:
     if not os.path.exists('logs'):
--- config.py
+++ config.py
@@ -12,8 +12,13 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = True
-    POSTS_PER_PAGE = 3
-
+    POSTS_PER_PAGE = 10
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['unnamed200@gmail.com']
 
 class ProductionConfig(Config):
     DEBUG = False
